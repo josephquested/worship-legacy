08d1956c88d06c3a4e90066ff075c259a6395e96
josephquested <josephquested@gmail.com>
2016-06-21 14:20:49 +1200

    update camera transitioning function

--- Assets/Scripts/Game/CameraController.cs
+++ Assets/Scripts/Game/CameraController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CameraController : MonoBehaviour
 {
 	private Transform screen;
+	[SerializeField] private bool transitioning;
   private float lerpTime;
 
 	public float speed = 1.0F;
@@ -16,9 +17,18 @@ public class CameraController : MonoBehaviour
 
 	public void SetScreen (Transform newScreen)
 	{
+		SetTransitioning(newScreen);
 		if (screen == newScreen) return;
 		screen = newScreen;
 		lerpTime = Time.time;
+	}
+
+	void SetTransitioning (Transform newScreen)
+	{
+		transitioning = !(
+			transform.position.x == newScreen.position.x
+			&& transform.position.y == newScreen.position.y
+		);
 	}
 
 	void Update ()
@@ -30,4 +40,8 @@ public class CameraController : MonoBehaviour
 		if (float.IsNaN(fracJourney)) return;
     transform.position = Vector3.Lerp(transform.position, target, fracJourney);
   }
+
+	public bool Transitioning {
+		get { return transitioning; }
+	}
 }
