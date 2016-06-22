206e2afc0a016ab1ab8765f2ae997258745287eb
josephquested <josephquested@gmail.com>
2016-06-23 09:46:08 +1200

    add meat sprite update functionality

---
+++ Assets/Scripts/Actor/Player/HUDMeat.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDMeat : MonoBehaviour
+{
+	private SpriteRenderer spriteRenderer;
+
+	[SerializeField] private Sprite[] sprites;
+	[SerializeField] private int plump;
+
+	void Start ()
+	{
+		spriteRenderer = this.GetComponent<SpriteRenderer>();
+	}
+
+	void Update ()
+	{
+		UpdateSprite();
+	}
+
+	public void ChangePlump (int value)
+	{
+		if (value > 0) plump += value;
+		if (value < 0) plump -= value;
+		UpdateSprite();
+	}
+
+	void UpdateSprite () {
+		spriteRenderer.sprite = sprites[plump];
+	}
+}
