f4b6ec3518d1cdb1cfbf2cb497f7dee6a0ebe417
josephquested <josephquested@gmail.com>
2016-06-22 19:41:48 +1200

    add attack script

--- Assets/Scripts/Item/Attack.cs
+++ Assets/Scripts/Item/Attack.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attack : MonoBehaviour {
+public class Attack : MonoBehaviour
+{
+	[SerializeField] Sprite[] sprites;
+	[SerializeField] int direction;
 
-	// Use this for initialization
-	void Start () {
-	
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		print(collider.gameObject.name);
+		// collider send message
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public int Direction
+	{
+		get { return direction; }
+		set { direction = value; }
 	}
 }
