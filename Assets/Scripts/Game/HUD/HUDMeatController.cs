5f576b04415c56914935dbb51883e2aa17875577
josephquested <josephquested@gmail.com>
2016-06-23 20:17:21 +1200

    add HUD meat controller

---
+++ Assets/Scripts/Game/HUD/HUDMeatController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDMeatController : MonoBehaviour
+{
+	[SerializeField] private HUDMeat[] hudMeat;
+
+	void Start ()
+	{
+		UpdateBones();
+	}
+
+	void UpdateBones ()
+	{
+		for (int i = 1; i < hudMeat.Length; i++)
+		{
+			hudMeat[i].transform.localPosition = new Vector2(
+				hudMeat[i - 1].transform.localPosition.x + 1,
+				0
+			);
+		}
+	}
+
+	void UpdateMeat ()
+	{
+		
+	}
+}
